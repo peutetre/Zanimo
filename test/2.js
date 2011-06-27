d2d82d7fc3b7ddcc5573adc430faf332d15147d3
Paul Panserrieu <ppa@zenexity.com>
2011-06-27 23:10:08 +0200

    recursive loop test

---
+++ test/2.js
@@ -0,0 +1,40 @@
+(function (win, doc) {
+
+    function echo(msg) {
+        return function (val) { 
+            console.log("value: " + val + " message: " + msg); 
+            return val; 
+        };
+    }
+
+    function $(el) {
+        return doc.getElementById(el);
+    }
+
+    function tests() {
+        var elt = $('square1');
+        
+        (function loop() {
+            Zanimo.when(Zanimo.transition(elt, "width", "300px", 1000, "ease-in"))
+                  .then(function () { 
+                            Zanimo.when(Zanimo.transition(elt, "width", "100px", 1000, "ease-in"))
+                                  .then(loop, echo("Error loop width fails!!!")); 
+                  });
+        })();
+
+
+        (function loop() {
+            Zanimo.when(Zanimo.transition(elt, "height", "300px", 1000, "ease-in"))
+                  .then(function () { 
+                            Zanimo.when(Zanimo.transition(elt, "height", "100px", 1000, "ease-in"))
+                                  .then(loop, echo("Error loop height fails!!!")); 
+                  });
+        })();
+    }
+
+    doc.addEventListener("DOMContentLoaded", function () {
+        console.log("DOMContentLoaded");
+        tests();
+    });
+
+})(window, window.document);
