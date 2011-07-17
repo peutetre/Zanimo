9681ce54b617e709e64505eadbac31601005e365
Paul Panserrieu <ppa@zenexity.com>
2011-07-17 16:57:12 +0200

    better test template...

--- test/template.js
+++ test/template.js
@@ -1,18 +1,18 @@
 (function (win, doc) {
 
-    function echo(msg) {
-        return function (val) { 
-            console.log("value: " + val + " message: " + msg); 
-            return val; 
-        };
+    function $(id) {
+        return doc.getElementById(id);
     }
 
-    function tests() {
-        console.log("Starting tests ???? ");
+    function test() {
+        Zanimo.when(Zanimo.delay(1000, $("square1")), function (elt) { 
+            return Zanimo.transition(elt, "width", "300px", 1000, "ease-in"); 
+        }).then(function(elt) { return Zanimo.transition(elt, "height", "300px", 1000, "ease-in") } )
+          .then(function(elt) { return Zanimo.transition(elt, "height", "100px", 1000, "ease-in") } ) 
     }
 
     doc.addEventListener("DOMContentLoaded", function () {
-        tests();
+        test();
     }, false);
 
 }(window, window.document));
