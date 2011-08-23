271dbabf47f1ab014a8a6d74227c1d51fa056c5c
Paul Panserrieu <ppa@zenexity.com>
2011-08-24 00:20:15 +0200

    splitting the test.js in two parts
    * the test
    * some utilites

--- examples/template/test.js
+++ examples/template/test.js
@@ -1,29 +1,4 @@
-(function (doc) {
-
-    var logsContainer = $("logs");
-
-    function $(id) {
-        return doc.getElementById(id);
-    }
-
-    function rejectAndlog(tag) {
-        return function (raison) {
-            var d = Zanimo.async.defer();
-            d.resolve(Zanimo.async.reject(tag + " : " + raison));
-            return d.promise;
-        }
-    }    
-
-    function done() {
-        return function (elt) {
-            logsContainer.innerHTML += '<span class="greenText">' + "Done with : " + elt.id  + '</span><br>';
-        };
-    }
-
-    function fail (tag) {
-        return function (r) {
-            logsContainer.innerHTML += '<span class="redText">' +   tag + ":" + r + '</span><br>'; }
-    }
+(function (doc, utils) {
 
     function withTo300In1sec () {
         return function (elt) {
@@ -32,13 +7,13 @@
     }
 
     function firstStep() {
-        return Zanimo.transition($('square1'), "background-color", "green", 1000, "ease-in");
+        return Zanimo.transition(utils.$('square1'), "background-color", "green", 1000, "ease-in");
     }
 
     function secondStep() {
         return function (elt) {
             return Zanimo.transition(elt, "width", "300px", 1000, "ease-in")
-                         .then( withTo300In1sec(), rejectAndlog("won't be called...") );
+                         .then( withTo300In1sec(), utils.rejectAndlog("won't be called...") );
         };
     }
 
@@ -48,21 +23,11 @@
         };
     }
 
-    function test() {
-        Zanimo.when( firstStep(), secondStep(), rejectAndlog("will be called after that when you click the test button...") )
-              .then( thirdStep(), rejectAndlog("Oups... second step failed..."))
-              .then( done(), fail("failed with raison => ") );
+    // The test...
+    window.test = function () {
+        Zanimo.when( firstStep(), secondStep(), utils.rejectAndlog("will be called after that when you click the test button...") )
+              .then( thirdStep(), utils.rejectAndlog("Oups... second step failed..."))
+              .then( utils.done(), utils.fail("failed with raison => ") );
     }
 
-
-    function reset() {
-        $("square1").style.cssText = " ";
-        //logsContainer.innerHTML = " "; 
-    }
-
-    doc.addEventListener("DOMContentLoaded", function () {
-        $("trigger-test-button").addEventListener("click", test, false);
-        $("trigger-reset-button").addEventListener("click", reset, false);
-    }, false);
-
-}(window.document));
+}(window.document, window.utils));
