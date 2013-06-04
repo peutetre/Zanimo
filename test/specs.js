7fd7da3eda51701a946b45347b9acbbcd1cc2a3b
Paul Panserrieu <ppa@zenexity.com>
2013-06-05 01:04:33 +0200

    [tests] start tests on `ready` + try some TAPE console output

--- test/specs.js
+++ test/specs.js
@@ -57,6 +57,20 @@
         });
     };
 
+    function tapeOutput(rslt) {
+        console.log('TAP version 13');
+        console.log('1..'+(rslt.length-1));
+        console.log('# Zanimo.js test suite');
+        rslt.forEach(function (r, idx) {
+            if (r.indexOf("✔") === 0) {
+                console.log('ok ' + (idx+1) + ' ' + r)
+            }
+            else if(idx !== rslt.length-1) {
+                console.log('not ok ' + (idx+1) + ' ' + r)
+            }
+        });
+    }
+
     function init () {
 
         var startbtn = document.getElementById("start"),
@@ -79,6 +93,14 @@
         clearbtn.addEventListener("click", function () {
             result.innerHTML = "";
         }, false);
+
+        // start tests
+        testLock = true;
+        start().done(function(r) {
+            browserlog(r);
+            tapeOutput(r);
+            testLock = false;
+        });
     }
 
     window.document.addEventListener("DOMContentLoaded", init, false);
