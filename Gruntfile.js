50c86a0eedf855b04452a3e797717aafa3cd5b33
Paul Panserrieu <ppa@zenexity.com>
2013-09-05 17:45:18 +0200

    update saucelabs test browsers

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,67 +1,4 @@
 module.exports = function(grunt) {
-    var browsers = [{
-        browserName: "chrome",
-        platform: "linux"
-    },{
-        browserName: "chrome",
-        platform: "Windows 7"
-    },{
-        browserName: "googlechrome",
-        platform: "Windows XP"
-    },{
-        browserName: "internet explorer",
-        platform: "Windows 8",
-        version: "10"
-    },{
-        browserName: "firefox",
-        platform: "Windows 8",
-        version: "21"
-    },{
-        browserName: "firefox",
-        platform: "Windows 8",
-        version: "20"
-    },{
-        browserName: "firefox",
-        platform: "Windows 8",
-        version: "19"
-    },{
-        browserName: "firefox",
-        platform: "Windows 8",
-        version: "18"
-    },{
-        browserName: "firefox",
-        platform: "Windows 8",
-        version: "17"
-    },{
-        browserName: "firefox",
-        platform: "Windows 8",
-        version: "16"
-    },{
-        browserName:"iphone",
-        platform: "OS X 10.8",
-        version: "6"
-    },{
-        browserName:"iphone",
-        platform: "OS X 10.8",
-        version: "5.1"
-    },{
-        browserName:"iphone",
-        platform: "OS X 10.6",
-        version: "5.0"
-    },{
-        browserName:"safari",
-        platform: "OS X 10.8",
-        version: "6"
-    },{
-        browserName:"safari",
-        platform: "OS X 10.6",
-        version: "5"
-    },{
-        browserName:"android",
-        platform: "Linux",
-        version: "4.0"
-    }];
-
     grunt.initConfig({
         connect: {
             server: {
@@ -78,7 +15,7 @@ module.exports = function(grunt) {
                     tunnelTimeout: 5,
                     build: (new Date()).getTime(),
                     concurrency: 3,
-                    browsers: browsers,
+                    browsers: grunt.file.readJSON('browsers.json').browsers,
                     testname: "zanimo tests",
                     testReadyTimeout:30000,
                     tags: ["master"]
